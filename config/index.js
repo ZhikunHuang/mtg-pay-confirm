501d48de6368b248b59bbf9981956ebbc0927ea6
BluckHuang <Bluck.Huang@developertools.com>
2024-02-02 15:05:14 +0800

    deploy js done

--- config/index.js
+++ config/index.js
@@ -42,7 +42,7 @@ module.exports = {
 
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
-    assetsSubDirectory: 'mtg-pay-confirm',
+    assetsSubDirectory: 'mtg-pay-confirm/dist/static',
     assetsPublicPath: '/',
 
     /**
